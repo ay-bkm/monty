0eb60920a6b48f04dce75abebe3c924b1e707ba0
Ayoub <ay.belkacem@gmail.com>
2023-10-20 01:25:18 +0100

    0x19. C - Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -40,5 +40,6 @@ typedef struct instruction_s
 void monty_push(stack_t **stack, unsigned int line_number);
 void monty_pall(stack_t **stack, unsigned int line_number);
 void monty_pint(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *stack);
 
 #endif /* MONTY_H */
